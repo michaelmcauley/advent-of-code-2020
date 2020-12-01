e72f89e62b86f9899ff2e6a005f397e24140f20b
Michael McAuley <michael.mcauley@rallyhealth.com>
2020-12-01 13:02:01 -0500

    day 1 part 2

--- day-01/puzzle-1.js
+++ day-01/puzzle-1.js
@@ -15,5 +15,23 @@ function findPair() {
   }
 }
 
+function findTriad() {
+  for (var i = 0; i < numbers.length; i++) {
+    const num1 = parseInt(numbers[i]);
+    for (var j = i + 1; j < numbers.length; j++) {
+      const num2 = parseInt(numbers[j]);
+      for (var k = j + 1; k < numbers.length; k++) {
+        const num3 = parseInt(numbers[k]);
+        if (num1 + num2 + num3 === 2020) {
+          return [num1, num2, num3];
+        }
+      }
+    }
+  }
+}
+
 const pair = findPair();
 console.log(pair[0] * pair[1]);
+
+const triad = findTriad();
+console.log(triad[0] * triad[1] * triad[2]);
