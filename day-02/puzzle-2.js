780713037b031de7e911614479a828cf3b6a549a
Michael McAuley <michael.mcauley@rallyhealth.com>
2020-12-02 06:09:53 -0500

    day 2 part 1

---
+++ day-02/puzzle-2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const list = fs.readFileSync("puzzle-2-input.txt").toString().split("\n");
+
+const parsedList = list.map((listEntry) => {
+  const pattern = /(\d+)\-(\d+)\s(\w):\s(\w+)/;
+  const matches = pattern.exec(listEntry);
+  return {
+    char: matches[3],
+    min: parseInt(matches[1]),
+    max: parseInt(matches[2]),
+    password: matches[4],
+  };
+});
+
+let validPasswords = 0;
+
+parsedList.forEach((listEntry) => {
+  const occurrences = listEntry.password.match(new RegExp(listEntry.char, "g"));
+  const numOccurrences = occurrences ? occurrences.length : 0;
+  if (numOccurrences <= listEntry.max && numOccurrences >= listEntry.min) {
+    validPasswords += 1;
+  }
+});
+
+console.log(validPasswords);
