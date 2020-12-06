84a62f8c5d2d5f0cfa05cca43a2e7c785e9995d0
Mike McAuley <mike.mcauley@icloud.com>
2020-12-06 14:11:09 -0500

    day 6 part 2

--- day-06.js
+++ day-06.js
@@ -1,15 +1,27 @@
 const { parseFileToLines } = require("./utils/file-utils.js")
 const { describe, it, expect } = require("./utils/test-utils.js")
 
-function sumCountsAcrossGroups(inputFilename) {
+function sumCountsAcrossGroups(inputFilename, countingMode) {
   const lines = parseFileToLines(`${__dirname}/inputs/${inputFilename}`)
-  let count = 0, yesAnswers = new Set()
+  let count = 0, yesAnswers = new Set(), isFirstInGroup = true
   for (let i = 0; i <= lines.length; i++) {
     if (!lines[i]) {
       count += yesAnswers.size
       yesAnswers.clear()
+      isFirstInGroup = true
     } else {
-      lines[i].split('').map(question => yesAnswers.add(question))
+      if (countingMode === 'any' || isFirstInGroup) {
+        lines[i].split('').map(question => yesAnswers.add(question))
+      } else {
+        const yesAnswersThisLine = new Set(lines[i].split(''))
+        const yesAnswersPrior = Array.from(yesAnswers)
+        yesAnswersPrior.forEach(question => {
+          if (!yesAnswersThisLine.has(question)) {
+            yesAnswers.delete(question)
+          }
+        })
+      }
+      isFirstInGroup = false
     }
   }
   return count
@@ -18,10 +30,17 @@ function sumCountsAcrossGroups(inputFilename) {
 describe('day 6', () => {
   describe('part 1', () => {
     it('getsCorrectAnswer', () => {
-      expect(sumCountsAcrossGroups('day-06.example.txt')).toEqual(11)
+      expect(sumCountsAcrossGroups('day-06.example.txt', 'any')).toEqual(11)
+    })
+  })
+  describe('part 2', () => {
+    it('getsCorrectAnswer', () => {
+      expect(sumCountsAcrossGroups('day-06.example.txt', 'all')).toEqual(6)
     })
   })
 })
 
-const countsAcrossAllGroups = sumCountsAcrossGroups('day-06.txt')
-console.log({ countsAcrossAllGroups })
+const countsAnyAcrossAllGroups = sumCountsAcrossGroups('day-06.txt', 'any')
+const countsAllAcrossAllGroups = sumCountsAcrossGroups('day-06.txt', 'all')
+console.log({ countsAnyAcrossAllGroups })
+console.log({ countsAllAcrossAllGroups })
